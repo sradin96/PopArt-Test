362d2f504cafc7c74a84296c9884b859767919ca
sradin96 <sradin96@gmail.com>
2021-09-30 14:54:21 +0200

    Tablet mode finished

--- script/script.js
+++ script/script.js
@@ -5,3 +5,12 @@ $(".temporary__cta").on("click", function() {
 $('.header__link').on('click', function () {
 	$('.header__link').removeClass('active').eq($(this).index('.header__link')).addClass('active');
 });
+
+$('.header__menu').on('click', function() {
+    $('.header__navigation-block').toggleClass('active-menu');
+    if($(".header__navigation-block").hasClass("active-menu")) {
+		$('body').addClass('fixed-position');
+	} else {
+		$('body').removeClass('fixed-position');
+	}
+});
